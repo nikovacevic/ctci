1e8debb80d13eabb2568681b4f42b8020dedc835
Niko Kovacevic <nikovacevic@gmail.com>
2016-11-20 11:36:49 -0700

    Implemented a constructor for IntStack. Made IntStack threadsafe.

--- stack/stack.go
+++ stack/stack.go
@@ -1,5 +1,7 @@
 package stack
 
+import "sync"
+
 // Stack defines behavior of a stack data structure
 type Stack interface {
 	Pop() interface{}
@@ -8,22 +10,44 @@ type Stack interface {
 	IsEmpty() bool
 }
 
-type intStack []int
+// IntStack implements a thread-safe Stack of type int
+type IntStack struct {
+	lock  sync.Mutex
+	stack []int
+}
 
-func (s *intStack) Pop() int {
-	n := (*s)[len(*s)-1]
-	(*s) = (*s)[0 : len(*s)-1]
+// NewIntStack creates and returns a reference to a new instance of an IntStack
+func NewIntStack() *IntStack {
+	return &IntStack{stack: []int{}}
+}
+
+// Pop removes and returns the top element in the IntStack.
+func (s *IntStack) Pop() int {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	n := s.stack[len(s.stack)-1]
+	s.stack = s.stack[0 : len(s.stack)-1]
 	return n
 }
 
-func (s *intStack) Push(n int) {
-	*s = append(*s, n)
+// Push takes one or more ints, pushing each onto the IntStack in the given
+// order; e.g. Push(1, 2, 3) causes 3 to be the top element.
+func (s *IntStack) Push(n ...int) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.stack = append(s.stack, n...)
 }
 
-func (s *intStack) Peek() int {
-	return (*s)[len(*s)-1]
+// Peek returns the value of the top element, but does not remove it.
+func (s *IntStack) Peek() int {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	return s.stack[len(s.stack)-1]
 }
 
-func (s *intStack) IsEmpty() bool {
-	return len(*s) == 0
+// IsEmpty returns true if the IntStack has no elements.
+func (s *IntStack) IsEmpty() bool {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	return len(s.stack) == 0
 }
