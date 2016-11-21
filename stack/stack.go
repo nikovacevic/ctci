004cc77435768fb3d546cb3a37efa52bc37e18fc
Niko Kovacevic <nikovacevic@gmail.com>
2016-11-21 09:24:58 -0700

    Min 3.2 complete

--- stack/stack.go
+++ stack/stack.go
@@ -5,6 +5,8 @@ import (
 	"sync"
 )
 
+const maxInt = 1<<31 - 1
+
 // Stack defines behavior of a stack data structure
 type Stack interface {
 	Push(interface{})
@@ -15,42 +17,54 @@ type Stack interface {
 
 // IntStack implements a thread-safe Stack of type int
 type IntStack struct {
-	lock  sync.Mutex
-	stack []int
+	lock     sync.Mutex
+	stack    []int
+	minstack []int
 }
 
 // NewIntStack creates and returns a reference to a new instance of an IntStack
 func NewIntStack() *IntStack {
-	return &IntStack{stack: []int{}}
+	return &IntStack{
+		stack:    make([]int, 0),
+		minstack: []int{maxInt},
+	}
 }
 
 // Push takes one or more ints, pushing each onto the IntStack in the given
 // order; e.g. Push(1, 2, 3) causes 3 to be the top element.
-func (s *IntStack) Push(n ...int) {
+func (s *IntStack) Push(nums ...int) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
-	s.stack = append(s.stack, n...)
+	for _, n := range nums {
+		s.stack = append(s.stack, n)
+		s.minstack = append(s.minstack, s.minstack[len(s.minstack)-1])
+		if n < s.minstack[len(s.minstack)-1] {
+			s.minstack[len(s.minstack)-1] = n
+		}
+	}
 }
 
 // Pop removes and returns the top element in the IntStack.
 func (s *IntStack) Pop() (int, error) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
 	if s.IsEmpty() {
 		return 0, fmt.Errorf("Stack is empty")
 	}
-	n := s.stack[len(s.stack)-1]
-	s.stack = s.stack[0 : len(s.stack)-1]
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	i := len(s.stack) - 1
+	n := s.stack[i]
+	s.stack = s.stack[0:i]
+	s.minstack = s.minstack[0 : len(s.minstack)-1]
 	return n, nil
 }
 
 // Peek returns the value of the top element, but does not remove it.
 func (s *IntStack) Peek() (int, error) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
 	if s.IsEmpty() {
 		return 0, fmt.Errorf("Stack is empty")
 	}
+	s.lock.Lock()
+	defer s.lock.Unlock()
 	return s.stack[len(s.stack)-1], nil
 }
 
@@ -59,4 +73,18 @@ func (s *IntStack) IsEmpty() bool {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 	return len(s.stack) == 0
+}
+
+func (s *IntStack) String() string {
+	return fmt.Sprintf("%v", s.stack)
+}
+
+// Min (2.2) returns the values of the minimum element
+func (s *IntStack) Min() (int, error) {
+	if s.IsEmpty() {
+		return 0, fmt.Errorf("Stack is empty")
+	}
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	return s.minstack[len(s.minstack)-1], nil
 }
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -1,1 +1,37 @@
 package stack
+
+import (
+	"fmt"
+	"testing"
+)
+
+var minTests = []struct {
+	push []int
+	pop  int
+	min  int
+	err  error
+}{
+	{[]int{1, 2, 3}, 1, 1, nil},
+	{[]int{5, 2, 3, 2, 1}, 1, 2, nil},
+	{[]int{}, 0, 0, fmt.Errorf("Stack is empty")},
+}
+
+func TestMin(t *testing.T) {
+	for _, tt := range minTests {
+		stack := NewIntStack()
+		stack.Push(tt.push...)
+		for i := 0; i < tt.pop; i++ {
+			_, _ = stack.Pop()
+		}
+		min, err := stack.Min()
+		if tt.err != nil {
+			if fmt.Sprintf("%v", tt.err) != fmt.Sprintf("%v", err) {
+				t.Errorf("%v.Min() expected error \"%v\", actual \"%v\"", stack, tt.err, err)
+			}
+		} else {
+			if min != tt.min {
+				t.Errorf("%v.Min() expected \"%v\", actual \"%v\"", stack, tt.min, min)
+			}
+		}
+	}
+}
